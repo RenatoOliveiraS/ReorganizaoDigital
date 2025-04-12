98a6ab5f9485e815a612a385349afb941ac52ab5
Renato Oliveira <zyx.renato@gmail.com>
2025-04-12 16:15:33 -0300

    Alterando 3 linhas print

--- src/helloword.py
+++ src/helloword.py
@@ -2,5 +2,6 @@
 def main():
     print("Hello, Teste de QUARTO commit !")
     print("Hello, Teste de TESTE commit !")
+    print("Hello, Teste de mais commit !")
 if __name__ == "__main__":
     main()
