88a98597eaf8df95d405c6f511d1375d00598c24
Renato Oliveira <zyx.renato@gmail.com>
2025-04-12 16:21:15 -0300

    Retirando as linhas

--- src/helloword.py
+++ src/helloword.py
@@ -1,7 +1,6 @@
 # src/hello_world.py
 def main():
     print("Hello, Teste de QUARTO commit !")
-    print("Hello, Teste de TESTE commit !")
-    print("Hello, Teste de mais commit !")
+
 if __name__ == "__main__":
     main()
